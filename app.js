e26d51bab0641d92e0d2cf729c27958215f0b4d0
Alan Chang <allan850920@hotmail.com>
2020-11-15 19:10:51 +0800

    feat: add todo seeder

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
+const Restaurant = require('./models/restaurant')
 
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -23,22 +23,7 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantList.results })
-})
 
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword.trim().toLowerCase()
-  const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.trim().toLowerCase().includes(keyword) || restaurant.category.trim().toLowerCase().includes(keyword)
-  })
-  res.render('index', { restaurants: restaurants, keyword: keyword })
-})
-
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurant: restaurant })
-})
 
 app.listen(port, () => {
   console.log(`Express is listening on http://localhost:${port}`)
