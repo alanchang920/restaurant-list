a11d117b8b18a70acf735a958a3a0f302e653f1f
Alan Chang <allan850920@hotmail.com>
2020-11-15 18:41:03 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
 
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
+
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
